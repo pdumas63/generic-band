365ad6789452d50a0546035cdf607d7344a7cb5e
pdumas63 <pdumas@stagiaire-humanbooster.com>
2021-07-19 11:07:56 +0200

    replace files by wamp's files

--- content/index.php
+++ content/index.php
@@ -1,64 +1,57 @@
 <!DOCTYPE html>
-<?php
-require_once 'sql/Sql.php';
-$sql = new Sql;
-$band = $sql->band();
-?>
-
 <html>
 
 <head>
-    <link rel="icon" type="image/jpeg" href="assets/img/<?php echo $band['logo']; ?>" />
-    <title><?php echo $band['name']; ?></title>
+    <link rel="icon" type="image/jpeg" href="assets/img/image_298512.jpg" />
+    <title>Les 3 Stagiaires Maléfiques</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/sidebar.css">
 
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">   
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
 
-<body>
+<body>    
     <header>
-
+    
         <!-- ArrowUp -->
-        <div id="arrowup">
-            <a href="#" title="Haut de Page"><img src="assets/img/neon/ArrowUp.png"></img></a>
-        </div>
+    <div id="arrowup">
+        <a href="#" title="Haut de Page"><img src="assets/img/neon/ArrowUp.png"></img></a>
+    </div>
         <div id="wrapper">
             <div class="overlay"></div>
 
             <!-- Sidebar -->
             <nav class="navbar-inverse fixed-top" id="sidebar-wrapper" role="navigation">
-                <div class="responsive-cross">
-                    <button type="button" class="hamburger animated fadeInLeft is-closed" data-toggle="offcanvas">
-                        <span class="hamb-top"></span>
-                        <span class="hamb-middle"></span>
-                        <span class="hamb-bottom"></span>
-                    </button>
-                </div>
+            <div class="responsive-cross">
+                <button type="button" class="hamburger animated fadeInLeft is-closed" data-toggle="offcanvas">
+                    <span class="hamb-top"></span>
+                    <span class="hamb-middle"></span>
+                    <span class="hamb-bottom"></span>
+                </button>
+            </div>
                 <ul class="nav sidebar-nav">
                     <div class="sidebar-header">
                         <div class="sidebar-brand">
                             <a href="#">
                                 <div class="menu">
-                                    <img src="assets/img/<?php echo $band['logo']; ?>" style="width: 100px;">
+                                    <img src="assets/img/PascalLogo.png" style="width: 100px;">
                                 </div>
                             </a>
                         </div>
-                    </div>
+                    </div>                    
                     <li><a href="#recordings">NOTRE SON</a></li>
-                    <li><a href="#news">NOTRE ACTU</a></li>
+                    <li><a href="#news">NOTRE ACTU</a></li>                                 
                     <li><a href="#videos">NOS VIDEOS</a></li>
                     <li><a href="#concerts">NOS CONCERTS</a></li>
                     <li><a href="#contact">CONTACTEZ-NOUS</a></li>
-                    <li><a href="form/login-form.php">SE CONNECTER</a></li>
-                </ul>
+                </ul>    
             </nav>
             <!-- /#sidebar-wrapper -->
 
@@ -87,7 +80,7 @@ $band = $sql->band();
                             <div class="sign-line"></div>
                         </div>
                         <div class="sign-title">NIWEE</div>
-                        <div class="sign-title">Prod.</div>
+                        <div class="sign-title">Prod.</div>                         
                         <div class="sign-lines">
                             <div class="sign-line"></div>
                             <div class="sign-line"></div>
@@ -132,14 +125,14 @@ $band = $sql->band();
                             <div class="grid-cells"></div>
                             <div class="grid-cells"></div>
                         </div>
-                    </div>
+                    </div>                    
                     <div class="show-details">présente<br>
-                        <span style="color: #000000;"><?php echo $band['name']; ?></span>
+                        <span style="color: #000000;">Les 3 Stagiaires Maléfiques</span>
                     </div>
                 </div>
             </div>
         </section>
-        <!-- section "Notre Son" -->
+        <!-- section "Notre Son" -->        
         <div class="section bg-white" id="recordings">
             <div class="container">
                 <h2 class="section-title">Notre Son</h2>
@@ -148,22 +141,26 @@ $band = $sql->band();
                         <img src="assets/img/shawn-sim-9lfubBqLbRc-unsplash.jpg" alt="batterie" />
                         <figcaption>
                             <h3>Cliquez ICI pour MUSIQUE</h3>
-                        </figcaption>
+                        </figcaption>                            
                     </figure>
                 </div>
-                <div class="lecteur">                    
-                    <iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=57336617/size=large/bgcol=333333/linkcol=e99708/tracklist=false/transparent=true/" seamless><a href="https://peaceville.bandcamp.com/album/the-roundhouse-tapes">The Roundhouse Tapes by Opeth</a></iframe>
-                
-                    <iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=117944312/size=large/bgcol=333333/linkcol=e99708/tracklist=false/transparent=true/" seamless><a href="https://seasonofmistcatalogue.bandcamp.com/album/xiii">XIII by Trepalium</a></iframe>
-                
-                    <iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=1949509316/size=large/bgcol=333333/linkcol=e99708/tracklist=false/transparent=true/" seamless><a href="https://detorn.bandcamp.com/album/the-dark-passenger">The Dark Passenger by Detorn</a></iframe>                
-                </div>
+                <div class="lecteur">
+                    <div>
+                        <iframe style="border: 0; width: 100%; height: 100%;" src="https://bandcamp.com/EmbeddedPlayer/album=57336617/size=large/bgcol=333333/linkcol=e99708/tracklist=false/artwork=small/track=510499622/transparent=true/" seamless><a href="https://peaceville.bandcamp.com/album/the-roundhouse-tapes">The Roundhouse Tapes by Opeth</a></iframe>
+                    </div>
+                    <div>
+                        <iframe style="border: 0; width: 100%; height: 100%;" src="https://bandcamp.com/EmbeddedPlayer/album=117944312/size=large/bgcol=333333/linkcol=e99708/tracklist=false/artwork=small/track=673988765/transparent=true/" seamless><a href="https://seasonofmistcatalogue.bandcamp.com/album/xiii">XIII by Trepalium</a></iframe>
+                    </div>
+                    <div>
+                        <iframe style="border: 0; width: 100%; height: 100%;" src="https://bandcamp.com/EmbeddedPlayer/album=1949509316/size=large/bgcol=333333/linkcol=e99708/tracklist=false/artwork=small/track=450826400/transparent=true/" seamless><a href="https://detorn.bandcamp.com/album/the-dark-passenger">The Dark Passenger by Detorn</a></iframe>
+                    </div>                    
+                </div>               
             </div>
         </div>
         <!-- section "Notre Actu" -->
         <div class="section bg-black" id="actus">
-            <div class="container">
-                <h2 id="news" class="section-title">Notre Actu</h2>
+            <div class="container">               
+                <h2 id="news" class="section-title">Notre Actu</h2>                              
                 <!-- sous-section "Groupe" -->
                 <div id="groupe">
                     <div>
@@ -171,27 +168,36 @@ $band = $sql->band();
                             <img src="assets/img/gros-plan-guitare-acoustique-beau-fond-colore_169016-3530.jpg" alt="guitare" />
                             <figcaption>
                                 <h3>GROUPE</h3>
-                            </figcaption>
+                            </figcaption>                                
                         </figure>
                     </div>
                     <div class="textGroupe">
-                        <p><?php echo $band['qui']; ?></p>
+                        <p>Opeth est un groupe de death metal progressif suédois, originaire de Stockholm. Fondé en 1990, le style d'Opeth, basé en grande partie sur la présence continue du chanteur et guitariste Mikael Åkerfeldt, est bien ancré dans la tradition heavy metal. Le groupe est également sous-classé dans le death metal et le rock progressif (ou une combinaison de ces genres) selon l'album et parfois la chanson à laquelle le critique se réfère. D'une part, Opeth emprunte aux styles les plus brutaux à travers des chants gutturaux et des séquences instrumentales très agressives tandis que de l'autre, il se distingue par des séquences progressives mélodiques, très souvent douces et envoûtantes. Cette apparente dichotomie est présente dans tous les albums du groupe bien qu'à des degrés différents, ce qui rend sa classification dans un style musical d'autant plus complexe.</p>
+                        <p>Membres actuels
+
+                            Mikael Åkerfeldt – guitare rythmique (depuis 1990), chant (depuis 1992)
+                            Martín Méndez – guitare basse (depuis 1997)
+                            Martin « Axe » Axenrot – batterie, percussion (2005–2006 : musicien de session39 ; depuis 2006 : membre officiel)
+                            Fredrik Åkesson – guitare solo, chœurs (depuis 2007)
+                            Joakim Svalberg – clavier, synthétiseur, chœurs, percussions (depuis 2011)</p>                        
                     </div>
                 </div>
-                <hr>
+                <hr> 
                 <!-- sous-section "Infos" -->
-                <div id="infos">
+                <div id="infos">                                              
                     <div>
                         <figure class="photo">
                             <img src="assets/img/jared-berg-HNxKoXrGgco-unsplash.jpg" alt="cymbale" />
                             <figcaption>
                                 <h3>INFOS</h3>
-                            </figcaption>
+                            </figcaption>                                
                         </figure>
                     </div>
-                    <div class="textInfo">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.</p>
-                    </div>
+                    <div class="textInfo">                    
+                        <p>Au cours de sa longue carrière, Opeth fidélise et renforce sa base de fans, jusqu'à devenir un des groupes les plus connus du genre metal au cours des années 2000. L'album Damnation a su percer le classement Billboard 200 en entrant à la 192e position lors de son lancement en 20031. L'exploit est amélioré en septembre 2005 alors que Ghost Reveries passe deux semaines au classement aux positions 64 et 175 respectivement2.</p>
+
+                        <p>Opeth recense treize albums studio, deux albums live, un coffret et trois DVD musicaux. Le premier album du groupe, Orchid, est présenté en 1995. Le dernier album en date d'Opeth, In Cauda Venenum, est sorti le 27 septembre 2019.</p>
+                    </div>                          
                 </div>
                 <hr>
                 <!-- sous-section "Albums" -->
@@ -201,12 +207,12 @@ $band = $sql->band();
                             <img src="assets/img/kelsey-powers-abOAIvrD5_k-unsplash.jpg" alt="mur d'albums" />
                             <figcaption>
                                 <h3>Cliquez ICI pour ALBUMS</h3>
-                            </figcaption>
+                            </figcaption>                                
                         </figure>
                     </div>
-                    <div class="textAlbum">
+                    <div class="textAlbum">                    
                         <p>Albums studio
-                            <br>
+
                             1995 : Orchid
                             1996 : Morningrise
                             1998 : My Arms, Your Hearse
@@ -219,73 +225,73 @@ $band = $sql->band();
                             2011 : Heritage
                             2014 : Pale Communion
                             2016 : Sorceress
-                            2019 : In Cauda Venenum
-                            <br>                            
-                            Albums Live
-                            <br>
+                            2019 : In Cauda Venenum</p>
+                        <p>Live
+
                             2003 : Lamentations: Live at Shepherd's Bush Empire
                             2007 : The Roundhouse Tapes
                             2010 : In Live Concert at the Royal Albert Hall
                             2018 : Gardens of the Titans: Live at Red Rocks Amphitheatre</p>
-                    </div>
-                </div>
+                    </div>                    
+                </div>                             
             </div>
         </div>
 
         <!-- section videos -->
-
+        
         <div class="section bg-white" id="videos">
             <div class="container">
                 <div class="row">
-                    <h2 class="section-title text-dark">Nos Vidéos</h2>
+                    <h2 class="section-title text-dark">Vidéos</h2>
                     <div class="col-md-12">
                         <iframe src="https://www.youtube.com/embed/mYKLvYGqaC0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <div class="col-md-12">
                         <iframe src="https://www.youtube.com/embed/77EeUimiOro" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>                    
+                    </div>
+                    <div class="col-md-12">
+                        <iframe src="https://www.youtube.com/embed/CiWEKaa-ehM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
                 </div>
             </div>
         </div>
 
         <!-- section live dates & towns -->
         <div class="section bg-black" id="concerts">
-            <div class="container">
+            <div class="container">                
                 <div>
-                    <h2 class="section-title text-light">Nos Concerts</h2>
+                    <h2 class="section-title text-light">Concerts</h2>
                     <figure class="photo"><img src="/assets/img/arton106880.png" alt="fosse en délire">
                         <figcaption>
                             <h3>Cliquez ICI pour Dates & Villes</h3>
-                        </figcaption>
+                        </figcaption>                            
                     </figure>
                 </div>
-                <div class="textConcert">
+                <div class="textConcert">                   
                     <p>Paris-22/07/2022-Le Trianon
-                        <br>
-                        Clermont-Ferrand-30/07/2022-La Coopérative de Mai
-                        <br>
-                        Marseille-20/08/2022-Le Dôme
-                    </p>
-                </div>
+                    <br>
+                    Clermont-Ferrand-30/07/2022-La Coopérative de Mai
+                    <br>
+                    Marseille-20/08/2022-Le Dôme</p>
+                </div>                                    
             </div>
         </div>
 
         <!-- contact form -->
         <div class="section bg-white" id="contact">
-            <h2 class="section-title">Contactez-nous !</h2>
+        <h2 class="section-title">Contactez-nous !</h2>
 
-            <div class="container">
+            <div class="container" style="width: 800px;">
                 <div class="row">
                     <div>
-                        <?php require_once 'form/contact-form.php'; ?>
-                    </div>
+                        <?php require_once 'form/contact-form.php'; ?>                        
+                    </div>                    
                 </div>
             </div>
         </div>
     </main>
     <!-- footer -->
-    <?php require_once 'template/footer-basic.php'; ?>
-
+        <?php require_once 'template/footer-basic.php' ; ?>
 </body>
 
 </html>
